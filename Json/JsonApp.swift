0b1303f5f5d872edd2c37c6fd5428dca84e0b4f3
Vlad <vlad14196@gmail.com>
2023-02-22 17:12:50 +0300

    Initial Commit

---
+++ Json/JsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonApp.swift
+//  Json
+//
+//  Created by user on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct JsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
